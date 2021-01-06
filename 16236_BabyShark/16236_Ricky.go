749d175b8261dbedd62e7f83bfd5a41862437ccd
Ricky <ejb0329@gmail.com>
2021-01-06 19:00:29 +0900

    add queue

---
+++ 16236_BabyShark/16236_Ricky.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"Algorithm/16236_BabyShark/queue"
+	"fmt"
+)
+
+type sea [][]int
+
+func (s sea) String() string {
+	len := len(s)
+	var str string
+	for i := 0; i < len; i++ {
+		for j := 0; j < len; j++ {
+			str = str + fmt.Sprint(s[i][j]) + " "
+		}
+		if i != len-1 {
+			str += "\n"
+		}
+	}
+	return str
+}
+
+func main() {
+	var N, fish, sharkX, sharkY int
+	// count := 0
+	// shark := 2
+	q := queue.NewQueue()
+	fmt.Scanln(&N)
+
+	mySea := make(sea, N)
+	for i := 0; i < N; i++ {
+		mySea[i] = make([]int, N)
+	}
+
+	for i := 0; i < N; i++ {
+		for j := 0; j < N; j++ {
+			node := queue.Node{i, j}
+			q.enque(&node)
+			fmt.Scan(&fish)
+			if fish == 9 {
+				sharkX = i
+				sharkY = j
+			}
+			mySea[i][j] = fish
+		}
+	}
+	fmt.Println(mySea)
+	fmt.Println("Shark is at X: ", sharkX, " Y: ", sharkY)
+}
---
+++ 16236_BabyShark/queue/queue.go
@@ -0,0 +1,35 @@
+package queue
+
+import "fmt"
+
+type Node struct {
+	X, Y int
+}
+
+func (n *Node) String() string {
+	return fmt.Sprint(n.X, n.Y)
+}
+
+func NewQueue() *Queue {
+	return &Queue{}
+}
+
+type Queue struct {
+	nodes []*Node
+	count int
+}
+
+func (s *Queue) enque(n *Node) {
+	s.nodes = append(s.nodes[:s.count], n)
+	s.count++
+}
+
+func (s *Queue) deque() *Node {
+	if s.count == 0 {
+		return nil
+	}
+	s.count--
+	result := s.nodes[0]
+	s.nodes = s.nodes[1:]
+	return result
+}
