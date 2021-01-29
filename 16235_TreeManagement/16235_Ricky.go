6869bd0f9e896ffaee70d7123d57d4acee59ccb8
JuneBin-Eom <ejb0329@gmail.com>
2021-01-29 18:55:09 +0900

    Maximum 값에서 안풀림

--- 16235_TreeManagement/16235_Ricky.go
+++ 16235_TreeManagement/16235_Ricky.go
@@ -4,219 +4,117 @@ import (
 	"fmt"
 )
 
-type treeSlice []Tree
-
-type Tree struct {
-	x, y int
+type tree struct {
+	age, count int
 }
 
-type area struct {
-	nutrient int
-	trees    []treeSlice
-}
+type allAgeTrees []tree
 
 var N, M, K int
 var moveX, moveY []int
 
-// func spring(areas [][]area) [][]area {
-// 	for i := 0; i < N; i++ {
-// 		for j := 0; j < N; j++ {
-// 			for k, tree := range areas[i][j].trees {
-// 				if areas[i][j].nutrient >= tree.age {
-// 					areas[i][j].nutrient -= tree.age
-// 					areas[i][j].trees[k].age++
-// 					areas[i][j].trees[k].live = true
-// 				}
-// 			}
-// 		}
-// 	}
-// 	fmt.Println("====봄 후 areas 확인=========")
-// 	for j := 0; j < N; j++ {
-// 		fmt.Println(areas[j])
-// 	}
-// 	fmt.Println("=============================")
-// 	fmt.Println()
-// 	return areas
-// }
-
-// func summer(areas [][]area) [][]area {
-// 	for i := 0; i < N; i++ {
-// 		for j := 0; j < N; j++ {
-// 			dieIndex := 999
-// 			for k, tree := range areas[i][j].trees {
-// 				if !tree.live {
-// 					areas[i][j].nutrient += tree.age / 2
-// 					if k < dieIndex {
-// 						dieIndex = k
-// 					}
-// 				} else {
-// 					areas[i][j].trees[k].live = false
-// 				}
-// 			}
-// 			if dieIndex != 999 {
-// 				areas[i][j].trees = areas[i][j].trees[:dieIndex]
-// 			}
-// 		}
-// 	}
-// 	fmt.Println("====여름 후 areas 확인=========")
-// 	for j := 0; j < N; j++ {
-// 		fmt.Println(areas[j])
-// 	}
-// 	fmt.Println("=============================")
-// 	fmt.Println()
-// 	return areas
-// }
-
-// func fall(areas [][]area) [][]area {
-// 	for i := 0; i < N; i++ {
-// 		for j := 0; j < N; j++ {
-// 			for _, tree := range areas[i][j].trees {
-// 				if tree.age%5 == 0 {
-// 					for k := 0; k < 8; k++ {
-// 						if i+moveX[k] >= 0 && i+moveX[k] < N && j+moveY[k] >= 0 && j+moveY[k] < N {
-// 							areas[i+moveX[k]][j+moveY[k]].trees = append([]Tree{Tree{i + moveX[k], j + moveY[k], 1, false}}, areas[i+moveX[k]][j+moveY[k]].trees...)
-// 						}
-// 					}
-// 				}
-// 			}
-// 		}
-// 	}
-// 	fmt.Println("====가을 후 areas 확인=========")
-// 	for j := 0; j < N; j++ {
-// 		fmt.Println(areas[j])
-// 	}
-// 	fmt.Println("=============================")
-// 	fmt.Println()
-// 	return areas
-// }
-
-// func winter(areas [][]area, nutrients [][]int) [][]area {
-// 	for i := 0; i < N; i++ {
-// 		for j := 0; j < N; j++ {
-// 			areas[i][j].nutrient += nutrients[i][j]
-// 		}
-// 	}
-// 	fmt.Println("====겨울 후 areas 확인=========")
-// 	for j := 0; j < N; j++ {
-// 		fmt.Println(areas[j])
-// 	}
-// 	fmt.Println("=============================")
-// 	fmt.Println()
-// 	return areas
-// }
-
-func springAndSummer(areas [][]area) [][]area {
-	for i := 0; i < N; i++ {
-		for j := 0; j < N; j++ {
-			for k, treeArray := range areas[i][j].trees { // k가 현재 treeArray에 속한 나무들의 나이
-				if k != 0 && areas[i][j].nutrient >= k*len(treeArray) && len(treeArray) != 0 { // treeArray들이 모두 양분을 섭취할 만큼 양분 있음
-					fmt.Println(i, j, "에 있는", k, "살 나무들 영양분 버억")
-					areas[i][j].nutrient -= k * len(treeArray)
-				} else {
-					areas[i][j].nutrient += (k / 2) * len(treeArray)
-					areas[i][j].trees[k] = treeSlice{}
-				}
-			}
-			for k := len(areas[i][j].trees) - 2; k >= 0; k-- {
-				if len(areas[i][j].trees[k]) != 0 {
-					fmt.Println(i, j, "에 있는", k, "살 나무는 나이가 자랍니다")
-					areas[i][j].trees[k+1] = append(areas[i][j].trees[k+1], areas[i][j].trees[k]...)
-					areas[i][j].trees[k] = treeSlice{}
-					// fmt.Println(areas)
-				}
-			}
-		}
-	}
-	fmt.Println("====봄,여름 후 areas 확인=========")
-	for j := 0; j < N; j++ {
-		fmt.Println(areas[j])
-	}
-	fmt.Println("=============================")
-	fmt.Println()
-	return areas
-}
-
-func fallAndWinter(areas [][]area, nutrients [][]int) [][]area {
-	for i := 0; i < N; i++ {
-		for j := 0; j < N; j++ {
-			areas[i][j].nutrient += nutrients[i][j]
-			for index, _ := range areas[i][j].trees { // index가 나이
-				if index != 0 && index%5 == 0 && len(areas[i][j].trees) != 0 {
-					for k := 0; k < 8; k++ {
-						if i+moveX[k] >= 0 && i+moveX[k] < N && j+moveY[k] >= 0 && j+moveY[k] < N {
-							for l := 0; l < len(areas[i][j].trees[index]); l++ {
-								areas[i+moveX[k]][j+moveY[k]].trees[1] = append(areas[i+moveX[k]][j+moveY[k]].trees[1], Tree{i + moveX[k], j + moveY[k]})
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-	fmt.Println("====가을, 겨울 후 areas 확인=========")
-	for j := 0; j < N; j++ {
-		fmt.Println(areas[j])
-	}
-	fmt.Println("=============================")
-	fmt.Println()
-	return areas
-}
-
 func main() {
-
 	moveX = []int{-1, -1, -1, 0, 0, 1, 1, 1}
 	moveY = []int{-1, 0, 1, -1, 1, -1, 0, 1}
 	fmt.Scan(&N, &M, &K)
 
-	areas := make([][]area, N)
-	nutrients := make([][]int, N)
+	area := make([][]int, N)
+	S2D2 := make([][]int, N)
+	addition := make([][]int, N)
+	treeSlice := make([][]allAgeTrees, N)
 	for i := 0; i < N; i++ {
-		areas[i] = make([]area, N)
-		nutrients[i] = make([]int, N)
+		area[i] = make([]int, N)
+		addition[i] = make([]int, N)
+		S2D2[i] = make([]int, N)
+		treeSlice[i] = make([]allAgeTrees, N)
 		for j := 0; j < N; j++ {
 			var nutrient int
 			fmt.Scan(&nutrient)
-			areas[i][j].nutrient = 5
-			nutrients[i][j] = nutrient
-			areas[i][j].trees = make([]treeSlice, 10)
+			area[i][j] = 5
+			S2D2[i][j] = nutrient
 		}
 	}
 
 	for i := 0; i < M; i++ {
 		var x, y, age int
 		fmt.Scan(&x, &y, &age)
-		areas[x-1][y-1].trees[age] = append(areas[x-1][y-1].trees[age], Tree{x - 1, y - 1})
+		treeSlice[x-1][y-1] = append(treeSlice[x-1][y-1], tree{age, 1})
 	}
 
-	// fmt.Println("====입력 후 areas 확인=========")
-	// for j := 0; j < N; j++ {
-	// 	fmt.Println(areas[j])
-	// }
-	// fmt.Println("=============================")
-	// fmt.Println()
+	for year := 0; year < K; year++ {
+		for i := 0; i < N; i++ {
+			addition[i] = make([]int, N)
+		}
+		for i := 0; i < N; i++ {
+			for j := 0; j < N; j++ {
+				if len(treeSlice[i][j]) != 0 {
+					for index, t := range treeSlice[i][j] {
+						var alive, die int
+						if area[i][j] >= t.age*t.count { // 현재 age의 나무가 모두 섭취할 만큼의 양분이 있는 경우
+							area[i][j] -= t.age * t.count
+							treeSlice[i][j][index].age++
+						} else if area[i][j]/t.age != 0 { // 모든 나무가 양분을 섭취할 수 없는 경우(몇그루는 가능)
+							alive = area[i][j] / t.age      // 섭취할 수 있는 나무의 최대 수
+							die = t.count - alive           // 나머지는 다 죽음
+							area[i][j] -= alive * t.age     // 봄(양분 섭취)
+							area[i][j] += (t.age / 2) * die // 여름
+							treeSlice[i][j][index].age++
+							treeSlice[i][j][index].count = alive
+							treeSlice[i][j] = treeSlice[i][j][:index+1]
+							break
+						} else if area[i][j]/t.age == 0 { // 나무 모두 양분 섭취 불가능
+							area[i][j] += (t.age / 2) * t.count
+							treeSlice[i][j] = treeSlice[i][j][:index]
+							break
+						}
+					}
+				}
+			}
+		}
+		for i := 0; i < N; i++ {
+			for j := 0; j < N; j++ {
+				if len(treeSlice[i][j]) != 0 {
+					for _, t := range treeSlice[i][j] {
+						if t.age%5 == 0 { // 번식
+							for k := 0; k < 8; k++ {
+								if i+moveX[k] >= 0 && i+moveX[k] < N && j+moveY[k] >= 0 && j+moveY[k] < N {
+									addition[i+moveX[k]][j+moveY[k]] += t.count
+								}
+							}
+						}
+					}
+				}
+				area[i][j] += S2D2[i][j] // 겨울 양분 추가
+			}
+		}
 
-	// for i := 0; i < N; i++ {
-	// 	for j := 0; j < N; j++ {
-	// 		if len(areas[i][j].trees) > 1 {
-	// 			sort.Slice(areas[i][j].trees, func(n, m int) bool {
-	// 				return areas[i][j].trees[n].age < areas[i][j].trees[m].age
-	// 			})
-	// 		}
-	// 	}
-	// }
-
-	for i := 0; i < K; i++ {
-		areas = fallAndWinter(springAndSummer(areas), nutrients)
-		//areas = winter(fall(summer(spring(areas))), nutrients)
-		fmt.Println("한해가 지났습니다.")
+		for i := 0; i < N; i++ {
+			for j := 0; j < N; j++ {
+				if addition[i][j] != 0 {
+					treeSlice[i][j] = append([]tree{{1, addition[i][j]}}, treeSlice[i][j]...)
+				}
+			}
+		}
+		fmt.Println("========", year+1, "년후=======")
+		fmt.Println("현재 나무")
+		for i := 0; i < N; i++ {
+			fmt.Println(treeSlice[i])
+		}
 		fmt.Println()
+		fmt.Println("양분")
+		for i := 0; i < N; i++ {
+			fmt.Println(area[i])
+		}
+		fmt.Println("======================")
 	}
 
 	var result int
 	for i := 0; i < N; i++ {
 		for j := 0; j < N; j++ {
-			for k := 1; k < 10; k++ {
-				result += len(areas[i][j].trees[k])
+			if len(treeSlice[i][j]) != 0 {
+				for _, t := range treeSlice[i][j] {
+					result += t.count
+				}
 			}
 		}
 	}
